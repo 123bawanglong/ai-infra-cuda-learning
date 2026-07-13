d569c26f21f93619a32e1a9f773f776bb0863b0e
kk1 <kk1@users.noreply.github.com>
2026-07-13 16:39:12 +0800

    Implement

--- course4.1.cu
+++ course4.1.cu
@@ -1,51 +1,62 @@
 #include <iostream>
 #include <cuda_runtime.h>
-__global__ void reduce(float *g_idata,float *g_odata){
-    extern __shared__ float sdata[];
-    int tid=threadIdx.x;
-    sdata[tid]=g_idata[tid];
+#define BLOCK_SIZE 256
+__global__ void reduce(const float *g_idata,float *g_odata,int N){
+__shared__ float sidata[BLOCK_SIZE];
+int tid=threadIdx.x;
+int j=blockDim.x*blockIdx.x+threadIdx.x;
+if(j<N){
+sidata[tid]=g_idata[j];}
+else{
+sidata[tid]=0.f;
+}
+__syncthreads();
+for(int i=BLOCK_SIZE/2;i>=1;i/=2){
+    if(tid<i){
+        sidata[tid]+=sidata[tid+i];
+    }
     __syncthreads();
-    for(int s=1;s<blockDim.x;s*=2){
-        if(tid%(s*2)==0){
-            sdata[tid]+=sdata[tid+s];
-        }
-        __syncthreads();
-    }
-    if(tid==0){
-    g_odata[blockIdx.x]=sdata[0];
-    }
+}
+if(tid==0){
+    g_odata[blockIdx.x]=sidata[0];
+}
 }
 int main(){
     int N;
     std::cin>>N;
     float *h_idata=new float[N];
-    float h_odata=0.f;
     for(int i=0;i<N;i++){
         std::cin>>h_idata[i];
     }
     float *d_idata=nullptr;
     float *d_odata=nullptr;
+    int blockNum=(N+BLOCK_SIZE-1)/BLOCK_SIZE;
+    float* d_out=nullptr;
+    float h_out=0.f;
     cudaMalloc((void**)&d_idata,sizeof(float)*N);
-    cudaMalloc((void**)&d_odata,sizeof(float));
+    cudaMalloc((void**)&d_odata,sizeof(float)*blockNum);
+    cudaMalloc((void**)&d_out,sizeof(float));
     cudaMemcpy(d_idata,h_idata,sizeof(float)*N,cudaMemcpyHostToDevice);
     cudaEvent_t begin,end;
     cudaEventCreate(&begin);
     cudaEventCreate(&end);
     cudaEventRecord(begin);
-    reduce<<<1,N,sizeof(float)*N>>>(d_idata,d_odata);
+    reduce<<<blockNum,BLOCK_SIZE>>>(d_idata,d_odata,N);
+    reduce<<<1,BLOCK_SIZE>>>(d_odata,d_out,blockNum);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+    cudaMemcpy(&h_out,d_out,sizeof(float),cudaMemcpyDeviceToHost);
     cudaError_t err=cudaGetLastError();
     if(err!=cudaSuccess){
         std::cerr<<cudaGetErrorString(err)<<std::endl;
     }
-    cudaEventRecord(end);
-    cudaEventSynchronize(end);
     float time=0.f;
     cudaEventElapsedTime(&time,begin,end);
     std::cout<<time<<std::endl;
-    cudaMemcpy(&h_odata,d_odata,sizeof(float),cudaMemcpyDeviceToHost);
-    std::cout<<h_odata<<std::endl;
+    std::cout<<h_out<<std::endl;
     cudaFree(d_idata);
     cudaFree(d_odata);
+    cudaFree(d_out);
     delete[] h_idata;
     return 0;
 }
